3e409995d453e3608c0e818a0de8bea60a3202fb
kiven <kiven@tb-gaming.com>
2017-11-25 19:34:00 +0800

    给用户组和角色添加过滤功能

--- omsBackend/users/filters.py
+++ omsBackend/users/filters.py
@@ -2,7 +2,7 @@
 # author: kiven
 
 from django_filters import rest_framework as filters
-from users.models import User, Group
+from users.models import User, Group, Role
 
 class UserFilter(filters.FilterSet):
     class Meta:
@@ -14,4 +14,18 @@ class UserFilter(filters.FilterSet):
             'group__name': ['exact'],
             'is_active': ['exact'],
             'roles__cnname': ['exact'],
+        }
+
+class GroupFilter(filters.FilterSet):
+    class Meta:
+        model = Group
+        fields = {
+            'name': ['exact', 'contains'],
+        }
+
+class RoleFilter(filters.FilterSet):
+    class Meta:
+        model = Role
+        fields = {
+            'name': ['exact', 'contains'],
         }
--- omsBackend/users/views.py
+++ omsBackend/users/views.py
@@ -4,8 +4,8 @@
 from users.serializers import UserSerializer, GroupSerializer, RoleSerializer
 from rest_framework import viewsets
 
-from users.filters import UserFilter
-from users.models import Group, User, Role
+from users.filters import UserFilter, GroupFilter, RoleFilter
+from users.models import User, Group, Role
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -14,11 +14,14 @@ class UserViewSet(viewsets.ModelViewSet):
     filter_class = UserFilter
     ordering_fields = ('-create_date',)
 
+
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    filter_class = GroupFilter
 
 
 class RoleViewSet(viewsets.ModelViewSet):
     queryset = Role.objects.all()
     serializer_class = RoleSerializer
+    filter_class = RoleFilter
