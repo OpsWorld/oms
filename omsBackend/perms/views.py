537c4c691cd76a84024db1fcda4be9748fe8dd7d
kevin <kevin@126.com>
2017-12-04 20:09:37 +0800

    自定义ModelViewSet

--- omsBackend/perms/views.py
+++ omsBackend/perms/views.py
@@ -4,9 +4,14 @@
 from rest_framework import viewsets
 from perms.models import UserMenuPerms
 from perms.serializers import UserMenuPermsSerializer
+from rest_framework.response import Response
 
 class UserMenuPermsViewSet(viewsets.ModelViewSet):
     queryset = UserMenuPerms.objects.all()
     serializer_class = UserMenuPermsSerializer
 
+    def retrieve(self, request, *args, **kwargs):
+        serializer = UserMenuPermsSerializer(data=request.data)
+        print(serializer)
+        return Response({'something': 'my custom JSON'})
 
