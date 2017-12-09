71d389d06ea9dc7f2f1284c7c545732cfa817689
kevin <kevin@126.com>
2017-12-09 14:26:58 +0800

    删除无效代码

--- omsFrontendss/src/views/layout/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Topbar } from './Topbar';
-
-export { default as Levelbar } from './Levelbar';
-
-export { default as Sidebar } from './Sidebar';
-
-export { default as AppMain } from './AppMain';
