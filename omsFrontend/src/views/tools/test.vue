5f8fd17e9640a3d05a83dafc1901a4d6fec70fc1
kevin <kevin@126.com>
2017-12-13 16:31:21 +0800

    fix mail

--- omsFrontend/src/views/tools/test.vue
+++ omsFrontend/src/views/tools/test.vue
@@ -1,91 +1,51 @@
 <template>
-  <div>
-    <el-tree
-      :data="data2"
-      show-checkbox
-      default-expand-all
-      node-key="name"
-      ref="tree"
-      highlight-current
-      :props="defaultProps">
-    </el-tree>
-
-    <div class="buttons">
-      <el-button @click="getCheckedNodes">通过 node 获取</el-button>
-      <el-button @click="getCheckedKeys">通过 key 获取</el-button>
-      <el-button @click="setCheckedNodes">通过 node 设置</el-button>
-      <el-button @click="setCheckedKeys">通过 key 设置</el-button>
-      <el-button @click="resetChecked">清空</el-button>
-    </div>
-
-  </div>
+  <el-card style="width: 500px;margin: 20px;padding: 20px">
+    收件人: <el-input v-model="mailForm.to_list" placeholder="请输入收件人"></el-input>
+    抄送者: <el-input v-model="mailForm.cc_list" placeholder="请输入抄送者" disabled></el-input>
+    主 题: <el-input v-model="mailForm.sub" placeholder="请输入主题" disabled></el-input>
+    邮件内容: <el-input v-model="mailForm.context" placeholder="请输入邮件内容" disabled></el-input>
+    <el-button type="success" plain @click="sendMail">发送测试邮件</el-button>
+  </el-card>
 </template>
 <script>
-export default {
-  methods: {
-    getCheckedNodes() {
-      console.log(this.$refs.tree.getCheckedNodes())
-    },
-    getCheckedKeys() {
-      console.log(this.$refs.tree.getCheckedKeys())
-    },
-    setCheckedNodes() {
-      this.$refs.tree.setCheckedNodes([{
-        id: 5,
-        label: '二级 2-1'
-      }, {
-        id: 9,
-        label: '三级 1-1-1'
-      }])
-    },
-    setCheckedKeys() {
-      this.$refs.tree.setCheckedKeys([3])
-    },
-    resetChecked() {
-      this.$refs.tree.setCheckedKeys([])
-    }
-  },
+import { ws_url } from '@/config'
+import ElCard from '../../../../../omsFrontend/node_modules/element-ui/packages/card/src/main'
+import ElInput from '../../../../../omsFrontend/node_modules/element-ui/packages/input/src/input'
 
+export default {
+  components: {
+    ElInput,
+    ElCard
+  },
   data() {
     return {
-      data2: [{
-        id: 1,
-        label: '一级 1',
-        children: [{
-          id: 4,
-          label: '二级 1-1',
-          children: [{
-            id: 9,
-            label: '三级 1-1-1'
-          }, {
-            id: 10,
-            label: '三级 1-1-2'
-          }]
-        }]
-      }, {
-        id: 2,
-        label: '一级 2',
-        children: [{
-          id: 5,
-          label: '二级 2-1'
-        }, {
-          id: 6,
-          label: '二级 2-2'
-        }]
-      }, {
-        id: 3,
-        label: '一级 3',
-        children: [{
-          id: 7,
-          label: '二级 3-1'
-        }, {
-          id: 8,
-          label: '二级 3-2'
-        }]
-      }],
-      defaultProps: {
-        children: 'children',
-        label: 'label'
+      mailForm: {
+        to_list: 'kiven@tb-gaming.com',
+        cc_list: 'kiven@tb-gaming.com',
+        sub: 'test',
+        context: '我是一只小小鸟'
+      },
+      ws_stream: '/salt/sendmail/',
+      ws: ''
+    }
+  },
+  created() {
+    this.wsInit()
+  },
+  methods: {
+    sendMail() {
+      this.ws.send(JSON.stringify(this.mailForm))
+    },
+    wsInit() {
+      const self = this
+      self.ws = new WebSocket(ws_url + self.ws_stream)
+      if (self.ws.readyState === WebSocket.OPEN) self.ws.onopen()
+      self.ws.onmessage = (e) => {
+        this.$message({
+          type: e.code,
+          message: e.msg
+        })
+        // self.results.push(e.data);
       }
     }
   }
