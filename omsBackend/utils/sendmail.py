4cd7497760de05905843591d3c3472a08346aea7
kevin <kevin@126.com>
2017-12-14 16:05:05 +0800

    fix add mail

--- omsBackend/utils/sendmail.py
+++ omsBackend/utils/sendmail.py
@@ -24,7 +24,7 @@ def send_mail(to_list, cc_list, sub, content):
     msg['Subject'] = sub
     msg['From'] = me
     msg['To'] = to_list + ';'
-    msg['Cc'] = cc_list
+    msg['Cc'] = cc_list.replace(',', ';')
     list = msg['To']  + msg['Cc']
     html = """
     <html xmlns="http://www.w3.org/1999/xhtml">
@@ -40,6 +40,7 @@ def send_mail(to_list, cc_list, sub, content):
     </html>"""
     context = MIMEText(html, _subtype='html', _charset='utf-8')  # 解决乱码
     msg.attach(context)
+    print(list)
     try:
         send_smtp = smtplib.SMTP()
         send_smtp.connect(mail_host, 587)
