6916fa8a8435b6d486af14a4c8d2fe8c901343db
kevin <kevin@126.com>
2017-12-14 11:53:51 +0800

    fix

--- omsBackend/tools/views.py
+++ omsBackend/tools/views.py
@@ -33,7 +33,7 @@ class SendmailViewSet(viewsets.ModelViewSet):
                     cc_list = cc_list + c_email + ','
                 except Exception as e:
                     cc_list = cc_list
-        if cc_list.isspace():
+        if len(cc_list.split()):
             cc_list = 'kiven@tb-gaming.com'
         sub = request.data["sub"]
         content = request.data["content"]
