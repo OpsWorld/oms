50822709b692906dca0d23724873246dd726c5c3
kevin <kevin@126.com>
2017-12-18 19:21:22 +0800

    youyou

--- omsBackend/utils/sendmail.py
+++ omsBackend/utils/sendmail.py
@@ -36,7 +36,7 @@ def send_mail(to_list, cc_list, sub, content):
     <body>
     <div id="container">
     <h1>工单通知邮件</h1>
-    <a href='""" + content + """'>点我查看工单</a>
+    点击工单地址: <a href='""" + content + """'>""" + content + """"</a>
     </div>
     </body>
     </html>"""
