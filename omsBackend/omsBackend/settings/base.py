124cec1cae7f95131247011ae142d5556842d904
kevin <kevin@126.com>
2017-12-25 17:24:28 +0800

    add daphn

--- omsBackend/omsBackend/settings/base.py
+++ omsBackend/omsBackend/settings/base.py
@@ -199,24 +199,3 @@ LOGGING = {
     },
 }
 
-# Redis
-REDIS_OPTIONS = {
-    'HOST': '127.0.0.1',
-    'PORT': 6379,
-    'DB': 0
-}
-
-USE_REDIS = True
-# Channel settings
-CHANNEL_LAYERS = {
-    "default": {
-        # 'BACKEND': 'asgiref.inmemory.ChannelLayer',    #如果使用这个，消息变多时，会读不出来
-         "BACKEND": "asgi_redis.RedisChannelLayer",
-         "CONFIG": {
-             "hosts": ['redis://{}:{}'.format(REDIS_OPTIONS['HOST'],
-                                              REDIS_OPTIONS['PORT'])]
-         },
-        "ROUTING": "omsBackend.routing.channel_routing"
-    }
-}
-
--- omsBackend/omsBackend/settings/prod.py
+++ omsBackend/omsBackend/settings/prod.py
@@ -44,3 +44,23 @@ LDAP_AUTH_URL = "ldap://192.168.6.101:389"
 LDAP_AUTH_SEARCH_BASE = "ou=AllUser,dc=tb-gaming,dc=local"
 LDAP_AUTH_CONNECTION_USERNAME = r'tb-gaming\itconfig'
 LDAP_AUTH_CONNECTION_PASSWORD = r'TUjweiAHZQ'
+
+# Redis
+REDIS_OPTIONS = {
+    'HOST': '127.0.0.1',
+    'PORT': 6379,
+    'DB': 0
+}
+USE_REDIS = True
+# Channel settings
+CHANNEL_LAYERS = {
+    "default": {
+        # 'BACKEND': 'asgiref.inmemory.ChannelLayer',    #如果使用这个，消息变多时，会读不出来
+         "BACKEND": "asgi_redis.RedisChannelLayer",
+         "CONFIG": {
+             "hosts": ['redis://{}:{}'.format(REDIS_OPTIONS['HOST'],
+                                              REDIS_OPTIONS['PORT'])]
+         },
+        "ROUTING": "omsBackend.routing.channel_routing"
+    }
+}
