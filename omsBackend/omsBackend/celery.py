84c03f0568c57898b7adca4379a4e6a127421f31
kevin <kevin@126.com>
2017-12-26 13:10:37 +0800

    add ws_client

--- omsBackend/omsBackend/celery.py
+++ omsBackend/omsBackend/celery.py
@@ -6,7 +6,7 @@ import os
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dailyblog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "omsBackend.settings")
 
 # 创建celery应用
 app = Celery('omsBackend')
