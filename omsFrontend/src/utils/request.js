0e023e07ef6e5f4adfb6c4de032b77aa9646d09c
kevin <kevin@126.com>
2017-12-26 18:36:02 +0800

    fix axios timeout

--- omsFrontend/src/utils/request.js
+++ omsFrontend/src/utils/request.js
@@ -4,12 +4,13 @@ import store from '@/store'
 
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+
 // axios.defaults.withCredentials = true
 
 // 创建axios实例
 const service = axios.create({
   baseURL: process.env.BASE_API, // api的base_url
-  timeout: 5000 // 请求超时时间
+  timeout: 8000 // 请求超时时间
 })
 
 // request拦截器
