74620dcd334e5560a2ca6d48fac1ce8943159796
kevin <kevin@126.com>
2018-01-09 19:28:14 +0800

    fix 过滤

--- omsFrontend/src/views/tools/test.vue
+++ omsFrontend/src/views/tools/test.vue
@@ -4,47 +4,45 @@
       <template slot="fc-header-left">
         <el-button type="primary" plain size="mini" icon="el-icon-plus" @click="addEvent=true">增加事件</el-button>
       </template>
-      <template slot="fc-body-card">
-        <el-button type="primary" plain size="mini" icon="el-icon-plus" @click="addEvent=true">增加事件</el-button>
-      </template>
     </full-calendar>
     <el-dialog :visible.sync="addEvent">
       <el-form :model="ruleForm" ref="ruleForm" label-width="100px" class="demo-ruleForm">
         <el-form-item label="标题" prop="title">
           <el-input v-model="ruleForm.title"></el-input>
         </el-form-item>
-        <el-form-item label="开始日期" firstDayOfWeek="1">
+        <el-form-item label="选择日期" firstDayOfWeek="1">
           <el-date-picker
-            v-model="start_date"
-            type="week"
-            format="yyyy 第 WW 周"
+            v-model="selectdate"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
             :picker-options="{
               firstDayOfWeek: 1
-            }"
-            placeholder="选择周">
+            }">
           </el-date-picker>
         </el-form-item>
-        <el-form-item label="开始时间" firstDayOfWeek="1">
-          <el-time-select
-            placeholder="起始时间"
-            v-model="startTime"
-            :picker-options="{
-              start: '08:00',
-              step: '01:00',
-              end: '24:00',
-            }">
-          </el-time-select>
-          <el-time-select
-            placeholder="结束时间"
-            v-model="endTime"
-            :picker-options="{
-              start: '08:00',
-              step: '01:00',
-              end: '24:00',
-              minTime: startTime
-            }">
-          </el-time-select>
-        </el-form-item>
+        <!--<el-form-item label="选择时间" firstDayOfWeek="1">-->
+          <!--<el-time-select-->
+            <!--placeholder="起始时间"-->
+            <!--v-model="startTime"-->
+            <!--:picker-options="{-->
+              <!--start: '08:00',-->
+              <!--step: '01:00',-->
+              <!--end: '24:00',-->
+            <!--}">-->
+          <!--</el-time-select>-->
+          <!--<el-time-select-->
+            <!--placeholder="结束时间"-->
+            <!--v-model="endTime"-->
+            <!--:picker-options="{-->
+              <!--start: '08:00',-->
+              <!--step: '01:00',-->
+              <!--end: '24:00',-->
+              <!--minTime: startTime-->
+            <!--}">-->
+          <!--</el-time-select>-->
+        <!--</el-form-item>-->
         <el-form-item label="类名" prop="cssClass">
           <el-input v-model="ruleForm.cssClass"></el-input>
         </el-form-item>
@@ -67,29 +65,28 @@ export default {
         title: '',
         start: '',
         end: '',
-        cssClass: 'yellow'
+        cssClass: 'jimmy'
       },
       events: [
         {
-          title: 'event1',
-          start: '2018-01-22',
-          cssClass: 'blue'
+          title: '8:00-10:00 jimmy',
+          start: '2018-01-15',
+          cssClass: 'jimmy'
         },
         {
-          title: 'event2',
-          start: '2018-01-12',
-          end: '2018-01-12',
-          cssClass: 'green'
+          title: '19:00-24:00 larry',
+          start: '2018-01-15',
+          cssClass: 'larry'
         }
       ],
-      start_date: '',
+      selectdate: '',
       startTime: '',
       endTime: ''
     }
   },
   methods: {
     eventClick(event, jsEvent, pos) {
-      console.log('eventClick', event, jsEvent, pos)
+      console.log('eventClick', event, pos)
     },
     addGroupSubmit(formName) {
       console.log(formName)
@@ -100,3 +97,13 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+  .jimmy {
+    background-color: #00ff8d !important;
+  }
+
+  .larry {
+    background-color: #c0dbff !important;
+  }
+</style>
