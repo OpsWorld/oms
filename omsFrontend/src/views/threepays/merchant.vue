064fef6ddb73a1d3d476ddea33020591e40f5329
kevin <kevin@126.com>
2018-01-12 18:07:31 +0800

    fix merchant

--- omsFrontend/src/views/threepays/merchant.vue
+++ omsFrontend/src/views/threepays/merchant.vue
@@ -21,10 +21,10 @@
           <el-table-column prop='domain' label='绑定域名'></el-table-column>
           <el-table-column prop='three' label='业务经理'></el-table-column>
           <!--<el-table-column label="操作">-->
-          <!--<template slot-scope="scope">-->
-          <!--<el-button @click="handleEdit(scope.row)" type="success" size="small">修改</el-button>-->
-          <!--<el-button @click="deleteGroup(scope.row.id)" type="danger" size="small">删除</el-button>-->
-          <!--</template>-->
+            <!--<template slot-scope="scope">-->
+              <!--<el-button @click="handleEdit(scope.row)" type="success" size="small">修改</el-button>-->
+              <!--<el-button @click="deleteGroup(scope.row.id)" type="danger" size="small">删除</el-button>-->
+            <!--</template>-->
           <!--</el-table-column>-->
         </el-table>
       </div>
@@ -40,15 +40,24 @@
         </el-pagination>
       </div>
     </el-card>
+    <!--<el-dialog :visible.sync="addForm">-->
+      <!--<add-group @formdata="addGroupSubmit"></add-group>-->
+    <!--</el-dialog>-->
+    <!--<el-dialog :visible.sync="editForm">-->
+      <!--<edit-group :rowdata="rowdata" @formdata="editGroupSubmit"></edit-group>-->
+    <!--</el-dialog>-->
   </div>
 </template>
 
 <script>
 import { getMerchant } from 'api/threeticket'
+// import { getMerchant, postMerchant, putMerchant, deleteMerchant } from 'api/threeticket'
 import { LIMIT } from '@/config'
+import addGroup from './components/addMerchant.vue'
+import editGroup from './components/editMerchant.vue'
 
 export default {
-  components: {},
+  components: { addGroup, editGroup },
   data() {
     return {
       tableData: [],
@@ -80,6 +89,48 @@ export default {
         this.tabletotal = response.data.count
       })
     },
+    //    addGroupSubmit(formdata) {
+    //      postMerchant(formdata).then(response => {
+    //        this.$message({
+    //          message: '恭喜你，添加成功',
+    //          type: 'success'
+    //        })
+    //        this.fetchData()
+    //        this.addForm = false
+    //      }).catch(error => {
+    //        this.$message.error('添加失败')
+    //        console.log(error)
+    //      })
+    //    },
+    //    editGroupSubmit(formdata) {
+    //      putMerchant(this.rowdata.id, formdata).then(response => {
+    //        this.$message({
+    //          message: '恭喜你，更新成功',
+    //          type: 'success'
+    //        })
+    //        this.fetchData()
+    //        this.editForm = false
+    //      }).catch(error => {
+    //        this.$message.error('更新失败')
+    //        console.log(error)
+    //      })
+    //    },
+    //    deleteGroup(id) {
+    //      deleteMerchant(id).then(response => {
+    //        this.$message({
+    //          message: '恭喜你，删除成功',
+    //          type: 'success'
+    //        })
+    //        this.fetchData()
+    //      }).catch(error => {
+    //        this.$message.error('删除失败')
+    //        console.log(error)
+    //      })
+    //    },
+    //    handleEdit(row) {
+    //      this.editForm = true
+    //      this.rowdata = row
+    //    },
     searchClick() {
       this.fetchData()
     },
