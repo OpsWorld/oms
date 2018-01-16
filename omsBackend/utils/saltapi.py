8f38dde5b45209190146ee6ba2a211c739eb9c42
kevin <kevin@126.com>
2018-01-16 11:38:49 +0800

    add cmdrun

--- omsBackend/utils/saltapi.py
+++ omsBackend/utils/saltapi.py
@@ -41,7 +41,7 @@ class SaltAPI(object):
         except KeyError:
             raise KeyError
 
-    def post_request(self, data, prefix='/'):
+    def salt_request(self, data, prefix='/'):
         url = self.__url + prefix
         self.__header["X-Auth-Token"] = self.__token
 
@@ -53,15 +53,20 @@ class SaltAPI(object):
 
         return req.json()
 
+    def remote_cmd(self, tgt, fun, client='local_async', expr_form='glob', arg=(), **kwargs):
+        data = {'client': client, 'fun': fun, 'tgt': tgt, 'expr_form': expr_form, 'arg': arg}
+        content = self.salt_request(data)
+        ret = content['return'][0]['jid']
+        return ret
+
     def check_jid(self, jid):
-        self.__header['X-Auth-Token'] = self.__token
         prefix = "{0}/{1}".format(restful["jobs"], jid)
-        content = self.post_request(None, prefix)
+        content = self.salt_request(None, prefix)
         return content
 
     def list_key(self):
         prefix = '/keys'
-        content = self.post_request(None, prefix)
+        content = self.salt_request(None, prefix)
         accepted = content['return']['minions']
         denied = content['return']['minions_denied']
         unaccept = content['return']['minions_pre']
@@ -70,20 +75,22 @@ class SaltAPI(object):
 
     def accept_key(self, key_id):
         data = {'client': 'wheel', 'fun': 'key.accept', 'match': key_id}
-        content = self.post_request(data)
+        content = self.salt_request(data)
         ret = content['return'][0]['data']['success']
         return ret
 
     def delete_key(self, key_id):
         data = {'client': 'wheel', 'fun': 'key.delete', 'match': key_id}
-        content = self.post_request(data)
+        content = self.salt_request(data)
         ret = content['return'][0]['data']['success']
         return ret
 
 
 def main():
     sapi = SaltAPI(url=salt_info["url"], username=salt_info["username"], password=salt_info["password"])
-    print(sapi.list_key())
+    jid = sapi.remote_cmd(tgt='sh-aa-01',fun='cmd.run',arg=('ls', '/'))
+    print(jid)
+    print(sapi.check_jid(jid))
 
 
 if __name__ == '__main__':
