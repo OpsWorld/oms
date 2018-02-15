163d0e0e1f31dca75b791af40909ad6359fd5b54
kevin <kevin@126.com>
2018-02-15 09:57:58 +0800

    fix is_public filter

--- omsBackend/projects/filters.py
+++ omsBackend/projects/filters.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # author: itimor
 
-from projects.models import Project, admin_groups
+from projects.models import Project, admin_groups, admin_users
 from users.models import User
 from django.db.models import Q
 from dry_rest_permissions.generics import DRYPermissionFiltersBase
@@ -13,12 +13,13 @@ class ProjectFilterBackend(DRYPermissionFiltersBase):
         Limits all list requests to only be seen by the create_groups.
         admin groups can get all().
         """
-        groups = User.objects.get(username=request.user).groups.all()
-        admin_list = [group.name for group in groups]
-
-        # 求交集
-        is_admin = [i for i in admin_list if i in admin_groups]
-        if len(is_admin) > 0:
+        # groups = User.objects.get(username=request.user).groups.all()
+        # admin_list = [group.name for group in groups]
+        #
+        # # 求交集
+        # is_admin = [i for i in admin_list if i in admin_groups]
+        # if len(is_admin) > 0:
+        if request.user in admin_users:
             return queryset
         else:
             return queryset.filter(
