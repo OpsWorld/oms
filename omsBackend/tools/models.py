c363e1468a549883bd57f78b70d01aea1f947bc6
kevin <kevin@126.com>
2018-03-09 10:38:59 +0800

    fix upload docx

--- omsBackend/tools/models.py
+++ omsBackend/tools/models.py
@@ -10,10 +10,10 @@ import os
 class Upload(models.Model):
     username = models.CharField(max_length=20, verbose_name=u'上传用户')
     file = models.FileField(upload_to=PathAndRename("./"), blank=True, verbose_name=u'上传文件')
-    archive = models.CharField(max_length=101, default=u'其他', null=True, blank=True, verbose_name=u'文件归档')
+    archive = models.CharField(max_length=201, default=u'其他', null=True, blank=True, verbose_name=u'文件归档')
     filename = models.CharField(max_length=201, null=True, blank=True, verbose_name=u'文件名')
     filepath = models.CharField(max_length=201, null=True, blank=True, verbose_name=u'文件路径')
-    type = models.CharField(max_length=30, null=True, blank=True, verbose_name=u'文件类型')
+    type = models.CharField(max_length=100, null=True, blank=True, verbose_name=u'文件类型')
     size = models.CharField(max_length=20, null=True, blank=True, verbose_name=u'文件大小')
     create_time = models.CharField(max_length=20, null=True, blank=True, verbose_name=u'文件日期')
 
