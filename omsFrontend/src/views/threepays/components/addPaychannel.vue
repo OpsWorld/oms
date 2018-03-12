d833cfe32e7d0e50d3e7ec04af205b7e87675d43
kevin <kevin@126.com>
2018-03-12 14:29:58 +0800

    fix threepay

--- omsFrontend/src/views/threepays/components/addPaychannel.vue
+++ omsFrontend/src/views/threepays/components/addPaychannel.vue
@@ -34,7 +34,7 @@
       </el-select>
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="submitForm('ruleForm')" :disabled="showtip">立即创建</el-button>
+      <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
       <el-button @click="resetForm('ruleForm')">重置</el-button>
     </el-form-item>
   </el-form>
