d3e11bc484460014bd5111ada0c64714fe99238e
kevin <kevin@126.com>
2018-03-24 12:10:41 +0800

    canncel auth

--- omsFrontend/src/views/jobs/components/runjob.vue
+++ omsFrontend/src/views/jobs/components/runjob.vue
@@ -13,7 +13,7 @@
             </el-steps>
 
             <div class="stepitem">
-              <el-form v-if="jobs.cur_step===0" :model="versionForm" :rules="svnrules" ref="versionForm"
+              <el-form v-if="jobs.cur_step===0" :model="versionForm" ref="versionForm"
                        label-width="90px">
                 <el-form-item label="发布版本" prop="version">
                   <el-input v-model="versionForm.version" :disabled="onlyread"></el-input>
@@ -151,259 +151,247 @@
   </div>
 </template>
 <script>
-import { getJob, patchJob, postDeployJob, getDeployenv, getDeploycmd } from '@/api/job'
-import { getDeployJob, deleteDeployJob, getUpdateJobsStatus } from '@/api/job'
-import { mapGetters } from 'vuex'
-import { LIMIT, pagesize, pageformat } from '@/config'
-import { postSendmessage } from '@/api/tool'
+  import {getJob, patchJob, postDeployJob, getDeployenv, getDeploycmd} from '@/api/job'
+  import {getDeployJob, deleteDeployJob, getUpdateJobsStatus} from '@/api/job'
+  import {mapGetters} from 'vuex'
+  import {LIMIT, pagesize, pageformat} from '@/config'
+  import {postSendmessage} from '@/api/tool'
 
-export default {
-  data() {
-    return {
-      route_path: this.$route.path.split('/'),
-      job_id: this.$route.params.job_id,
-      versionForm: {
-        version: '',
-        content: ''
-      },
-      ruleForm: {
-        job: '',
-        env: '',
-        deploy_hosts: [],
-        deploy_cmd: '',
-        action_user: localStorage.getItem('username'),
-        deploy_cmd_host: 'null'
-      },
-      svnrules: {
-        version: [
-          { required: true, message: '请输入正确的内容', trigger: 'blur' }
-        ]
-      },
-      steps: [],
-      cur_env: {},
-      cmds: [],
-      jobs: {},
-      sendnotice: true,
-      hasversion: false,
-      stepForm: {
-        cur_step: 1,
-        done: false
-      },
-      onlyread: false,
-      checkAll: true,
-      checkedcmds: [],
-      currentPage: 1,
-      listQuery: {
-        limit: LIMIT,
-        offset: '',
-        search: '',
-        job__id: ''
-      },
-      pagesize: pagesize,
-      pageformat: pageformat,
-      tableData: [],
-      tabletotal: 0,
-      DEPLOY_STATUS: {
-        deploy: { text: '发布中', type: 'primary', icon: 'el-icon-loading' },
-        success: { text: '发布成功', type: 'success', icon: 'el-icon-success' },
-        failed: { text: '发布失败', type: 'danger', icon: 'el-icon-error' }
-      },
-      selectId: [],
-      butstatus: true,
-      showresult: false,
-      job_results: [],
-      check_job_status: '',
-      deploy_cmds: []
-    }
-  },
-  computed: {
-    ...mapGetters([
-      'role'
-    ])
-  },
-  created() {
-    this.fetchJobData()
-    this.fetchDeployJobData()
-  },
-  methods: {
-    fetchJobData() {
-      const parmas = null
-      getJob(parmas, this.job_id).then(response => {
-        this.jobs = response.data
-        this.ruleForm.job = this.jobs.name
-        const parmas = {
-          job__id: this.job_id
-        }
-        getDeployenv(parmas).then(response => {
-          this.steps = response.data
-        })
-        const data = {
-          job__id: this.job_id,
-          level: this.jobs.cur_step
-        }
-        this.fetchJobenvData(data)
-      })
-    },
-    fetchJobenvData(parmas) {
-      getDeployenv(parmas).then(response => {
-        this.cur_env = response.data[0]
-        if (this.cur_env) {
-          this.ruleForm.env = this.cur_env.id
-          this.fetchDeploycmdData(this.cur_env.id)
-        }
-      })
-    },
-    fetchDeploycmdData(env) {
-      const parmas = {
-        env__id: env
+  export default {
+    data() {
+      return {
+        route_path: this.$route.path.split('/'),
+        job_id: this.$route.params.job_id,
+        versionForm: {
+          version: '',
+          content: ''
+        },
+        ruleForm: {
+          job: '',
+          env: '',
+          deploy_hosts: [],
+          deploy_cmd: '',
+          action_user: localStorage.getItem('username'),
+          deploy_cmd_host: 'null'
+        },
+        steps: [],
+        cur_env: {},
+        cmds: [],
+        jobs: {},
+        sendnotice: true,
+        hasversion: false,
+        stepForm: {
+          cur_step: 1,
+          done: false
+        },
+        onlyread: false,
+        checkAll: true,
+        checkedcmds: [],
+        currentPage: 1,
+        listQuery: {
+          limit: LIMIT,
+          offset: '',
+          search: '',
+          job__id: ''
+        },
+        pagesize: pagesize,
+        pageformat: pageformat,
+        tableData: [],
+        tabletotal: 0,
+        DEPLOY_STATUS: {
+          deploy: {text: '发布中', type: 'primary', icon: 'el-icon-loading'},
+          success: {text: '发布成功', type: 'success', icon: 'el-icon-success'},
+          failed: {text: '发布失败', type: 'danger', icon: 'el-icon-error'}
+        },
+        selectId: [],
+        butstatus: true,
+        showresult: false,
+        job_results: [],
+        check_job_status: '',
+        deploy_cmds: []
       }
-      getDeploycmd(parmas).then(response => {
-        this.checkedcmds = this.cmds = response.data
-      })
     },
-    submitForm(formdata) {
-      this.ruleForm.content = this.jobs.content
-      this.ruleForm.version = this.jobs.version
-      this.$refs[formdata].validate((valid) => {
-        if (valid) {
-          this.ruleForm.deploy_hosts = this.cur_env.deploy_hosts.join()
-          this.deploy_cmds = []
-          for (const item of this.checkedcmds) {
-            this.deploy_cmds.push(item.name)
+    computed: {
+      ...mapGetters([
+        'role'
+      ])
+    },
+    created() {
+      this.fetchJobData()
+      this.fetchDeployJobData()
+    },
+    methods: {
+      fetchJobData() {
+        const parmas = null
+        getJob(parmas, this.job_id).then(response => {
+          this.jobs = response.data
+          this.ruleForm.job = this.jobs.name
+          const parmas = {
+            job__id: this.job_id
           }
-          this.ruleForm.deploy_cmd = this.deploy_cmds.join('||')
-          this.ruleForm.deploy_path = this.jobs.deploy_path
-          postDeployJob(this.ruleForm).then(response => {
-            console.log(response.data.j_id)
-            this.fetchDeployJobData()
-          }).catch(error => {
-            this.$message.error('构建失败，请检查参数是否正确！')
-            console.log(error)
+          getDeployenv(parmas).then(response => {
+            this.steps = response.data
+          })
+          const data = {
+            job__id: this.job_id,
+            level: this.jobs.cur_step
+          }
+          this.fetchJobenvData(data)
+        })
+      },
+      fetchJobenvData(parmas) {
+        getDeployenv(parmas).then(response => {
+          this.cur_env = response.data[0]
+          if (this.cur_env) {
+            this.ruleForm.env = this.cur_env.id
+            this.fetchDeploycmdData(this.cur_env.id)
+          }
+        })
+      },
+      fetchDeploycmdData(env) {
+        const parmas = {
+          env__id: env
+        }
+        getDeploycmd(parmas).then(response => {
+          this.checkedcmds = this.cmds = response.data
+        })
+      },
+      submitForm(formdata) {
+        this.ruleForm.content = this.jobs.content
+        this.ruleForm.version = this.jobs.version
+        this.ruleForm.deploy_hosts = this.cur_env.deploy_hosts.join()
+        this.deploy_cmds = []
+        for (const item of this.checkedcmds) {
+          this.deploy_cmds.push(item.name)
+        }
+        this.ruleForm.deploy_cmd = this.deploy_cmds.join('||')
+        this.ruleForm.deploy_path = this.jobs.deploy_path
+        postDeployJob(this.ruleForm).then(response => {
+          console.log(response.data.j_id)
+          this.fetchDeployJobData()
+        }).catch(error => {
+          this.$message.error('构建失败，请检查参数是否正确！')
+          console.log(error)
+        })
+      },
+      changeCurstep() {
+        this.jobs.cur_step++
+        this.stepForm.done = this.jobs.done = false
+        if (this.jobs.cur_step > this.jobs.total_step) {
+          this.jobs.cur_step = this.stepForm.cur_step = 0
+          if (this.sendnotice) {
+            const messageForm = {
+              action_user: 'ITDept_SkypeID',
+              title: `【${this.jobs.name}】更新`,
+              message: `版本号: ${this.jobs.version}\n更新内容: ${this.jobs.content}\n操作人: ${this.ruleForm.action_user}`
+            }
+            postSendmessage(messageForm)
+          }
+          patchJob(this.job_id, this.stepForm).then(() => {
+            this.$router.push('/jobs/jobs')
           })
         } else {
-          console.log('error submit!!')
-          return false
+          this.stepForm.cur_step = this.jobs.cur_step
+          patchJob(this.job_id, this.stepForm).then(() => {
+            this.fetchJobData()
+          })
         }
-      })
-    },
-    changeCurstep() {
-      this.jobs.cur_step++
-      this.stepForm.done = this.jobs.done = false
-      if (this.jobs.cur_step > this.jobs.total_step) {
+      },
+      changeCurstepZero() {
+        this.onlyread = this.stepForm.done = this.jobs.done = false
         this.jobs.cur_step = this.stepForm.cur_step = 0
-        if (this.sendnotice) {
-          const messageForm = {
-            action_user: 'ITDept_SkypeID',
-            title: `【${this.jobs.name}】更新`,
-            message: `版本号: ${this.jobs.version}\n更新内容: ${this.jobs.content}\n操作人: ${this.ruleForm.action_user}`
+        patchJob(this.job_id, this.stepForm)
+      },
+      changeJobDone() {
+        this.onlyread = this.stepForm.done = this.jobs.done = true
+        patchJob(this.job_id, this.stepForm)
+      },
+      saveVersion(formdata) {
+        this.$refs[formdata].validate((valid) => {
+          if (valid) {
+            patchJob(this.job_id, this.versionForm)
+            this.onlyread = this.stepForm.done = this.jobs.done = true
           }
-          postSendmessage(messageForm)
-        }
-        patchJob(this.job_id, this.stepForm).then(() => {
-          this.$router.push('/jobs/jobs')
         })
-      } else {
-        this.stepForm.cur_step = this.jobs.cur_step
-        patchJob(this.job_id, this.stepForm).then(() => {
-          this.fetchJobData()
-        })
-      }
-    },
-    changeCurstepZero() {
-      this.onlyread = this.stepForm.done = this.jobs.done = false
-      this.jobs.cur_step = this.stepForm.cur_step = 0
-      patchJob(this.job_id, this.stepForm)
-    },
-    changeJobDone() {
-      this.onlyread = this.stepForm.done = this.jobs.done = true
-      patchJob(this.job_id, this.stepForm)
-    },
-    saveVersion(formdata) {
-      this.$refs[formdata].validate((valid) => {
-        if (valid) {
-          patchJob(this.job_id, this.versionForm)
-          this.onlyread = this.stepForm.done = this.jobs.done = true
-        }
-      })
-    },
-    handleCheckAllChange(val) {
-      this.checkedcmds = val ? this.cmds : []
-    },
-    handleCheckedcmdsChange(value) {
-      const checkedCount = value.length
-      this.checkAll = checkedCount === this.cmds.length
-    },
-    fetchDeployJobData() {
-      this.listQuery.job__id = this.job_id
-      getDeployJob(this.listQuery).then(response => {
-        this.tableData = response.data.results
-        this.tabletotal = response.data.count
-        const job_status = this.tableData.map(function(item) {
-          return item.deploy_status
-        })
-        if (job_status.indexOf('deploy') > -1) {
-          this.check_job_status = setInterval(() => {
-            const pramas = {
-              job__id: this.job_id
-            }
-            getUpdateJobsStatus(pramas).then(response => {
-              if (response.data.count === 0) {
-                clearInterval(this.check_job_status)
-                this.jobs.done = true
-                this.fetchDeployJobData()
-              } else {
-                console.log('check job_status 3/s')
+      },
+      handleCheckAllChange(val) {
+        this.checkedcmds = val ? this.cmds : []
+      },
+      handleCheckedcmdsChange(value) {
+        const checkedCount = value.length
+        this.checkAll = checkedCount === this.cmds.length
+      },
+      fetchDeployJobData() {
+        this.listQuery.job__id = this.job_id
+        getDeployJob(this.listQuery).then(response => {
+          this.tableData = response.data.results
+          this.tabletotal = response.data.count
+          const job_status = this.tableData.map(function (item) {
+            return item.deploy_status
+          })
+          if (job_status.indexOf('deploy') > -1) {
+            this.check_job_status = setInterval(() => {
+              const pramas = {
+                job__id: this.job_id
               }
-            })
-          }, 3000)
-        } else {
-          console.log('setInterval_id:' + this.check_job_status)
-          clearInterval(this.check_job_status)
-        }
-      })
-    },
-    handleSizeChange(val) {
-      this.listQuery.limit = val
-      this.fetchDeployJobData()
-    },
-    handleCurrentChange(val) {
-      this.listQuery.offset = (val - 1) * LIMIT
-      this.fetchDeployJobData()
-    },
-    handleSelectionChange(val) {
-      this.selectId = []
-      for (var i = 0, len = val.length; i < len; i++) {
-        this.selectId.push(val[i].id)
-      }
-      if (this.selectId.length > 0) {
-        this.butstatus = false
-      } else {
-        this.butstatus = true
-      }
-    },
-    deleteForm() {
-      clearInterval(this.check_job_status)
-      for (var i = 0, len = this.selectId.length; i < len; i++) {
-        deleteDeployJob(this.selectId[i]).then(response => {
-          delete this.selectId[i]
+              getUpdateJobsStatus(pramas).then(response => {
+                if (response.data.count === 0) {
+                  clearInterval(this.check_job_status)
+                  this.jobs.done = true
+                  this.fetchDeployJobData()
+                } else {
+                  console.log('check job_status 3/s')
+                }
+              })
+            }, 3000)
+          } else {
+            console.log('setInterval_id:' + this.check_job_status)
+            clearInterval(this.check_job_status)
+          }
         })
+      },
+      handleSizeChange(val) {
+        this.listQuery.limit = val
+        this.fetchDeployJobData()
+      },
+      handleCurrentChange(val) {
+        this.listQuery.offset = (val - 1) * LIMIT
+        this.fetchDeployJobData()
+      },
+      handleSelectionChange(val) {
+        this.selectId = []
+        for (var i = 0, len = val.length; i < len; i++) {
+          this.selectId.push(val[i].id)
+        }
+        if (this.selectId.length > 0) {
+          this.butstatus = false
+        } else {
+          this.butstatus = true
+        }
+      },
+      deleteForm() {
+        clearInterval(this.check_job_status)
+        for (var i = 0, len = this.selectId.length; i < len; i++) {
+          deleteDeployJob(this.selectId[i]).then(response => {
+            delete this.selectId[i]
+          })
+        }
+        setTimeout(this.fetchDeployJobData, 1000)
+      },
+      showJobResult(row) {
+        this.showresult = true
+        const data = (new Function('return ' + row))()
+        const a = []
+        Object.keys(data).map(function (k) {
+          a.push({'host': k, 'data': data[k]})
+        })
+        this.job_results = a
+      },
+      searchClick() {
+        this.fetchDeployJobData()
       }
-      setTimeout(this.fetchDeployJobData, 1000)
-    },
-    showJobResult(row) {
-      this.showresult = true
-      const data = (new Function('return ' + row))()
-      const a = []
-      Object.keys(data).map(function(k) {
-        a.push({ 'host': k, 'data': data[k] })
-      })
-      this.job_results = a
-    },
-    searchClick() {
-      this.fetchDeployJobData()
     }
   }
-}
 </script>
 
 <style lang='scss'>
