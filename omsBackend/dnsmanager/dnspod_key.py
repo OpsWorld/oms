4ee308e24320461ff5b0731878b980239e3f8db8
kevin <kevin@126.com>
2018-04-06 16:19:37 +0800

    ignore key.py

--- omsBackend/dnsmanager/dnspod_key.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# author: itimor
-
-
-DMSPOD_KEYINFO = {
-    "user": 'kevinloo@126.com',
-    "pwd": 'jay541430183',
-}
--- omsBackend/dnsmanager/godaddy_key.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# author: itimor
-
-# itimor
-GODADDY_KEYINFO = {
-    "name": 'test',
-    "key": 'dLDMrJikLukT_UbZYBYP4JM7Bu32pzHMMTd',
-    "secret": 'UbZaPp2fK2YTcpmGgDbzMV'
-}
