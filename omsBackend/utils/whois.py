70ed7e58eb455f4479eb95e2fa0c34badc875124
kevin <kevin@126.com>
2018-04-16 16:11:28 +0800

    fix whois

--- omsBackend/utils/whois.py
+++ omsBackend/utils/whois.py
@@ -10,13 +10,20 @@ def whois(domain):
     url = "http://whois.xinnet.com/php/domain_seo.php?domain=%s" % domain
     html = requests.get(url).text
     d = json.loads(html)
+    if d["whoisFlag"] == 'false':
+        create_time = datetime.now()
+        expire_time = datetime.now()
+        dnsService = 'www.itimor.cn'
+    else:
+        create_time = datetime.strptime(d["whois_registrantDate"],
+                                        get_time_format(d["whois_registrantDate"])) + timedelta(
+            hours=8)
+        expire_time = datetime.strptime(d["whois_expirationDate"],
+                                        get_time_format(d["whois_expirationDate"])) + timedelta(
+            hours=8)
+        dnsService = '|'.join(d["whois_dnsService"])
 
-    create_time = datetime.strptime(d["whois_registrantDate"], get_time_format(d["whois_registrantDate"])) + timedelta(
-        hours=8)
-    expire_time = datetime.strptime(d["whois_expirationDate"], get_time_format(d["whois_expirationDate"])) + timedelta(
-        hours=8)
-
-    return {"create_time": create_time, "expire_time": expire_time, "dnsService": '|'.join(d["whois_dnsService"])}
+    return {"create_time": create_time, "expire_time": expire_time, "dnsService": dnsService}
 
 
 def get_time_format(time):
@@ -29,4 +36,4 @@ def get_time_format(time):
 
 
 if __name__ == '__main__':
-    print(whois('github.cn'))
+    print(whois('itimor.com'))
